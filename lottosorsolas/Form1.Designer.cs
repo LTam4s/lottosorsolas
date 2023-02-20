07f725c8fefb96884be6ef59b5eea39b5e31184d
LTam4s <114070808+LTam4s@users.noreply.github.com>
2023-02-20 09:32:02 +0100

    Add project files.

---
+++ lottosorsolas/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace lottosorsolas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 330);
+            this.panel1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Image = global::lottosorsolas.Properties.Resources.shamrock_lucky_icon;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(213, 372);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 39);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Sorsolás";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(347, 360);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Nyerőszámok ->";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(347, 385);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Megjátszott ->";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Crimson;
+            this.label3.Location = new System.Drawing.Point(347, 408);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Találat ->";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ lottosorsolas/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lottosorsolas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<int> random = new List<int>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            List<string> list = new List<string>();
+            int r;
+            bool volte = false;
+            for (int i = 0; i < 5; i++)
+            {
+                r = rnd.Next(1, 90);
+                for (int j = 0; j < random.Count; j++)
+                {
+                    if (random[j] == r && volte != true)
+                    {
+                        volte = true;
+                        continue;
+                    }
+                }
+                if (volte == false)
+                {
+                    random.Add(r);
+                    volte = false;
+                }
+            }
+
+            panel1.Controls.Clear();
+            int size = 45;
+            int num = 1;
+            for (int i = 1; i < 7; i++)
+            {
+                for (int j = 1; j < 16; j++)
+                {
+                    Button Btn = new Button();
+                    Btn.SetBounds(j * size, i * size, size, size);
+                    Btn.Name = $"{i};{j}";
+                    Btn.Text = $"{num++}";
+                    Btn.BackColor = Color.LightGray;
+                    Btn.FlatStyle = FlatStyle.Flat;
+                    Btn.FlatAppearance.BorderSize = 0;
+                    Btn.Click += (o, oe) =>
+                    {
+                        Button bt = (Button)o;
+                        if (bt.BackColor == Color.Red)
+                        {
+                            bt.BackColor = Color.LightGray;
+                            label2.Text = Regex.Replace(label2.Text, $"{bt.Text}, ", "");
+                            
+                        }
+                        else if (bt.BackColor == Color.LightGray)
+                        {
+                            bt.BackColor = Color.Red;
+                            label2.Text += $"{bt.Text}, ";
+                        }
+                    };
+                    panel1.Controls.Add(Btn);
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string final = Regex.Replace(label2.Text, "Megjátszott ->", "");
+            final = Regex.Replace(final, " ", "");
+            string[] tomb = final.Split(',');
+            for (int i = 0; i < 5; i++)
+            {
+                label1.Text += $"{random[i]}, ";
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < tomb.Length; j++)
+                {
+                    if (tomb[j] == random[i].ToString())
+                    {
+                        label3.Text += $"{tomb[j]}, ";
+                    }
+                }
+            }
+            if (label3.Text == "Találat ->")
+            {
+                label3.Text = "Találat -> Nincs találat";
+            }
+        }
+    }
+}
